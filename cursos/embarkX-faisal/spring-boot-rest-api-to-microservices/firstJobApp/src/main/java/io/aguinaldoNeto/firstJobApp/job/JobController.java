f6ef381c3f7260fb243803f5a4d1d7097a24dd69
Aguinaldo Neto <neto_aguinaldo@outlook.com>
2025-01-12 11:20:55 -0300

    1 - CRUD Operations for Job

---
+++ cursos/embarkX-faisal/spring-boot-rest-api-to-microservices/firstJobApp/src/main/java/io/aguinaldoNeto/firstJobApp/job/JobController.java
@@ -0,0 +1,55 @@
+package io.aguinaldoNeto.firstJobApp.job;
+
+import io.aguinaldoNeto.firstJobApp.job.impl.JobServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+@RestController
+@RequestMapping("/jobs")
+public class JobController {
+
+    private final JobServiceImpl jobService;
+
+    public JobController(JobServiceImpl jobService) {
+        this.jobService = jobService;
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createJob(@RequestBody Job job) {
+        jobService.createJob(job);
+
+        return new ResponseEntity<>("Job added successfully", HttpStatus.CREATED);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Job> findJobById(@PathVariable Long id) {
+        Job jobId = jobService.findById(id);
+
+        if (nonNull(jobId))
+            return new ResponseEntity<>(jobId, HttpStatus.OK);
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteJobById(@PathVariable Long id) {
+        boolean delete = jobService.deleteJobById(id);
+
+        if (delete)
+            return new ResponseEntity<>("Job deleted successfully", HttpStatus.OK);
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+}
---
+++ cursos/embarkX-faisal/spring-boot-rest-api-to-microservices/firstJobApp/src/main/java/io/aguinaldoNeto/firstJobApp/job/JobService.java
@@ -0,0 +1,15 @@
+package io.aguinaldoNeto.firstJobApp.job;
+
+
+import java.util.List;
+
+public interface JobService {
+
+    List<Job> findAll();
+
+    void createJob(Job job);
+
+    Job findById(Long id);
+
+    boolean deleteJobById(Long id);
+}
